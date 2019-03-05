1acb51171b8257b98fdde37b968c2a8d8fba51d7
Fahad Alzahrani <fhdzhrani@gmail.com>
2019-03-05 18:42:41 +0300

    HW - W06D02
    It was a great, I found a tutorial and an example and this open a new way for me to write JS and jQuery in different way

--- exercises/stop-light/index.js
+++ exercises/stop-light/index.js
@@ -1,23 +1,32 @@
 function turnOffLights() {
     // change background color of stop light to red
 }
-// turn on stop function 
+// turn on stop function
 function turnOnStopLight() {
     // change background color of stop light to red
+    let stopLight = $("#stopLight");
+    stopLight.css("background","red");
 }
 
 // turn on slow function
-function turnOnSlowLight() { 
+function turnOnSlowLight() {
 // change background color of slow light to yellow
+    let slowLight = $("#slowLight");
+    slowLight.css("background","yellow");
 }
 
 // turn on go function
 function turnOnGoLight() {
     // change background color of go light to green
+      let goLight = $("#goLight");
+      goLight.css("background","green");
 }
 
 // set stop light click event
-
+$("#stopButton").on("click",turnOnStopLight);
 // set slow light click event
-
+$("#slowButton").on("click",turnOnSlowLight);
 // set go light click event
+$("#goButton").on("click",turnOnGoLight);
+
+// set turn off turnOffLights
--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,17 @@
-// click event for gray button
+// click gray oval
+ document.getElementById("grayButton").onclick = switchGray;
 
-// click event for white button
+// click white oval
+ document.getElementById("whiteButton").onclick = switchWhite;
 
+// function to change color to gray
 function switchGray() {
-    // change background to gray
-    // change font to white
+ document.getElementsByTagName("body")[0].style.backgroundColor = "gray";
+ document.getElementsByTagName("body")[0].style.color = "white";
 }
 
-function switchWhite() {
-    // change background to white
-    // change font to black
+// function to change color to white
+ function switchWhite() {
+ document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+ document.getElementsByTagName("body")[0].style.color = "black";
 }
